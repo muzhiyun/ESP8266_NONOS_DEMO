8ec243e3c3e0a4e035152416f7f2b01037e6b4cc
muzhiyun <my616422@126.com>
2018-10-04 23:47:35 +0800

    first commit

---
+++ user_main.c
@@ -0,0 +1,163 @@
+/*
+ * ESPRESSIF MIT License
+ *
+ * Copyright (c) 2016 <ESPRESSIF SYSTEMS (SHANGHAI) PTE LTD>
+ *
+ * Permission is hereby granted for use on ESPRESSIF SYSTEMS ESP8266 only, in which case,
+ * it is free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+#include "ets_sys.h"
+#include "osapi.h"
+#include "user_tcpclient.h"
+#include "user_interface.h"
+
+#include "user_devicefind.h"
+#include "user_webserver.h"
+
+#if ESP_PLATFORM
+#include "user_esp_platform.h"
+#endif
+
+uint32 priv_param_start_sec;
+
+/******************************************************************************
+ * FunctionName : user_rf_cal_sector_set
+ * Description  : SDK just reversed 4 sectors, used for rf init data and paramters.
+ *                We add this function to force users to set rf cal sector, since
+ *                we don't know which sector is free in user's application.
+ *                sector map for last several sectors : ABCCC
+ *                A : rf cal
+ *                B : rf init data
+ *                C : sdk parameters
+ * Parameters   : none
+ * Returns      : rf cal sector
+*******************************************************************************/
+uint32 ICACHE_FLASH_ATTR
+user_rf_cal_sector_set(void)
+{
+    enum flash_size_map size_map = system_get_flash_size_map();
+    uint32 rf_cal_sec = 0;
+
+    switch (size_map) {
+        case FLASH_SIZE_4M_MAP_256_256:
+            rf_cal_sec = 128 - 5;
+            priv_param_start_sec = 0x3C;
+            break;
+
+        case FLASH_SIZE_8M_MAP_512_512:
+            rf_cal_sec = 256 - 5;
+            priv_param_start_sec = 0x7C;
+            break;
+
+        case FLASH_SIZE_16M_MAP_512_512:
+            rf_cal_sec = 512 - 5;
+            priv_param_start_sec = 0x7C;
+            break;
+        case FLASH_SIZE_16M_MAP_1024_1024:
+            rf_cal_sec = 512 - 5;
+            priv_param_start_sec = 0xFC;
+            break;
+
+        case FLASH_SIZE_32M_MAP_512_512:
+            rf_cal_sec = 1024 - 5;
+            priv_param_start_sec = 0x7C;
+            break;
+        case FLASH_SIZE_32M_MAP_1024_1024:
+            rf_cal_sec = 1024 - 5;
+            priv_param_start_sec = 0xFC;
+            break;
+
+        case FLASH_SIZE_64M_MAP_1024_1024:
+            rf_cal_sec = 2048 - 5;
+            priv_param_start_sec = 0xFC;
+            break;
+        case FLASH_SIZE_128M_MAP_1024_1024:
+            rf_cal_sec = 4096 - 5;
+            priv_param_start_sec = 0xFC;
+            break;
+        default:
+            rf_cal_sec = 0;
+            priv_param_start_sec = 0;
+            break;
+    }
+
+    return rf_cal_sec;
+}
+
+void ICACHE_FLASH_ATTR hello_world(void)
+{
+	os_printf("\r\nHello world !\r\n");
+	//os_printf("The chip id is 0x%x \n", system_get_chip_id());
+	//int ret = os_get_random((unsigned char *)temp, 7);
+	//os_printf("ret %d, value 0x%08x%08x\n\r", ret, temp[1], temp[0]);
+
+}
+
+void ICACHE_FLASH_ATTR user_set_station_config(void)
+{
+	os_printf("\r\nHello world !\r\n");
+	char ssid[32]=SSID;
+	char password[64] = PASSWORD;
+	struct	station_config stationConf;
+	stationConf.bssid_set = 0;
+	os_memcpy(&stationConf.ssid, ssid, 32);
+	os_memcpy(&stationConf.password, password, 64);
+	wifi_station_set_config(&stationConf);
+	os_printf("\r\nHello world !\r\n");
+
+}
+
+void ICACHE_FLASH_ATTR
+user_rf_pre_init(void)
+{
+}
+
+/******************************************************************************
+ * FunctionName : user_init
+ * Description  : entry of user application, init user function here
+ * Parameters   : none
+ * Returns      : none
+*******************************************************************************/
+void ICACHE_FLASH_ATTR
+user_init(void)
+{
+	os_printf("SDK version:%s\n", system_get_sdk_version());
+	hello_world();    //尝试自调用函数
+	os_printf("The chip id is %x\n",system_get_chip_id() );
+	//打印芯片id 十六进制
+	os_printf("The default mode is %x\n",wifi_get_opmode_default());
+	//打印当前模式 十六进制
+	os_printf("now time is %x\n",system_get_time());
+	//打印当前时间
+	wifi_set_opmode(STATIONAP_MODE);
+	//设置热点+客户端模式
+	//三种模式
+	//STATIONAP_MODE
+	//SOFTAP_MODE
+	//STATION_MODE
+	wifi_station_set_hostname("ESP8266");
+	//设置主机名
+	user_set_station_config();
+	//配置客户端连接信息
+
+
+	//   tcpuser_init()；
+	//wifi_set_opmode(STATION_MODE);
+}
+
